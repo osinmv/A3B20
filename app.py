4476b68b45292a5621b2792f72ff1c4cb55e12f2
osinmv <osinmv@gmail.com>
2021-03-28 15:41:47 -0400

    Backend for database is finished
    All queries are tested.
    Also moved login  html and css files into appropriate folder. Renamed database and added required student1,student2 instructor1 instructor2 users to our database.

--- app.py
+++ app.py
@@ -1,11 +1,148 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, g
+import sqlite3
+DATABASE = './assignment3.db'
+
+
+def get_db():
+    """Return database"""
+    db = getattr(g, '_database', None)
+    if(db is None):
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+
+def make_dicts(cursor: sqlite3.Cursor, row: sqlite3.Row):
+    """Return data from querry as dictionary"""
+    return dict((cursor.description[idx][0], value)
+                for idx, value in enumerate(row))
+
+
+def update_db(query: str, args=()):
+    """Updates database and does not return a result"""
+    cursor = get_db().execute(query, args)
+    get_db().commit()
+    cursor.close()
+
+
+def query_db(query: str, args=(), one=None):
+    """Return result of query"""
+    if one is None:
+        one = True
+    else:
+        one = False
+    cursor = get_db().execute(query, args)
+    result = cursor.fetchall()
+    cursor.close()
+    return (result[0] if result else None) if one else result
+
+
+def get_all_marks():
+    """Return all marks for instructors"""
+    return query_db("""SELECT User.username,Assignment1,Assignment2,
+                    Assignment3, Assignment4, Midterm, FinalTest
+                    FROM User INNER JOIN StudentMark
+                    WHERE isInstructor==0
+                    AND User.username == StudentMark.username;""",
+                    (), one=False)
+
+
+def get_student_marks(username: str):
+    """Return username marks"""
+    return query_db("""SELECT * FROM StudentMark
+                    WHERE StudentMark.username == ?;""",
+                    (username,), one=True)
+
+
+def get_feedback(username: str):
+    """Return feedback by instructor"""
+    return query_db("""SELECT * FROM Feedback
+                    WHERE Feedback.username == ?;""",
+                    args=(username,), one=False)
+
+
+def get_regrade_requests():
+    """Return all regrade requests"""
+    return query_db("""SELECT * FROM RegradeRequests;""", (), one=False)
+
+
+def isUser(username: str, password: str):
+    """Return True if username is in database"""
+    result = query_db("""SELECT username FROM User WHERE User.username == ?
+                    AND User.password == ?;""",
+                      args=(username, password,))
+    if result is None:
+        return False
+    return True
+
+
+def isInstructor(username: str):
+    """Return true if username is instructor"""
+    result = query_db("""SELECT username FROM User WHERE User.username == ?
+                    AND User.isInstructor == 1;""",
+                      args=(username,))
+    if result is None:
+        return False
+    return True
+
+
+def addUser(username: str, password: str, isInstructor: int):
+    """Insert username to database"""
+    update_db("""INSERT INTO User VALUES (?,?,?);""",
+              args=(username, password, isInstructor,))
+
+
+def addUserMarks(username: str):
+    """Add username marks to database"""
+    update_db("""INSERT INTO StudentMark
+    VALUES (?,?,?,?,?,?,?);""", args=(username, 0, 0, 0, 0, 0, 0,))
+
+
+def addFeedback(username: str, content: str):
+    """Add content to database"""
+    update_db("""INSERT INTO Feedback VALUES (?,?);""",
+              args=(username, content,))
+
+
+def addRegradeRequest(username: str, content: str):
+    """Add regrade request to database"""
+    update_db("""INSERT INTO RegradeRequests VALUES (?, ?);""",
+              args=(username, content,))
+
+
+def updateUserMarks(username: str, marks: list):
+    """Update user marks"""
+    update_db("""UPDATE StudentMark SET Assignment1 = ?,
+    Assignment2 = ?,Assignment3 = ?
+    ,Assignment4 = ?,Midterm = ?
+    ,FinalTest = ? WHERE StudentMark.username == ?;""",
+              args=(marks[0], marks[1],
+                    marks[2], marks[3], marks[4], marks[5], username,))
+
 
 app = Flask(__name__)
 
 
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        # close the database if we are connected to it
+        db.close()
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # return render_template('index.html')
+    # addFeedback("Andy", "Very very good")
+    # addRegradeRequest("Max", "want to regrade second assignment")
+    # addUser("student3", "student3", 0)
+    # addUserMarks("student3")
+    updateUserMarks("student3", [1, 0, 1, 0, 0, 0])
+    # db = getattr(g, '_database', None)
+    # if db is not None:
+    # close the database if we are connected to it
+    #    db.close()
+    return render_template("index.html")
 
 
 @app.route('/announcement')
